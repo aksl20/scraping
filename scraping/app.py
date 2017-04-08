3fd6fb9d7b319f5b0b373eaa687a0f14f6ede755
aksl20 <axel.camara@hotmail.fr>
2017-04-08 21:10:32 +0200

    Init API with flask

--- scraping/app.py
+++ scraping/app.py
@@ -1,25 +1,38 @@
 #-*- coding: utf-8 -*-
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
-from json import dumps
 
 e = create_engine('sqlite:///rakuten.db')
 
 app = Flask(__name__)
 api = Api(app)
 
-class PSP(Resource):
-    def get(self):
-        #Connect to databse
-        conn = e.connect()
-        #Perform query and return JSON data
-        query = conn.execute("select * from PSP")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]} 
-        return result
+games = [
+    {
+        'id': 1,
+        'title': u'Nintendo 3DS',
+        'description': u'A lot of Nitendo 3DS games from rakuten.co.jp', 
+        'done': False
+    }
+]
+class GameListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True,
+            help = 'No task title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(TaskListAPI, self).__init__()
 
-api.add_resource(PSP, '/psp')
+    def get(self):
+        pass
+
+@app.route('/rakutenscraping/api/v1.0/games', methods=['GET'], endpoint = '/')
+def get_tasks():
+    return jsonify({'tasks':games})
+
+api.add_resource(GameListAPI, '/rakutenscraping/api/v1.0/games', endpoint = 'games')
 
 if __name__ == '__main__':
      app.run()
